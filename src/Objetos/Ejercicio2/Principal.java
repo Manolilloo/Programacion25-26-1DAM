4e622d6796b962833f9fbdff0e579fd8a5410f92
Manolilloo <mdemanuel2007@gmail.com>
2025-11-19 00:30:46 +0100

    creo que ya se arregló el cajero, pq no veas el tostón

--- src/Objetos/Ejercicio2/Principal.java
+++ src/Objetos/Ejercicio2/Principal.java
@@ -1,5 +1,7 @@
 package Objetos.Ejercicio2;
 
+import java.sql.SQLOutput;
+import java.util.MissingFormatArgumentException;
 import java.util.Scanner;
 
 public class Principal {
@@ -9,34 +11,52 @@ public class Principal {
         Cuenta c = new Cuenta();
         Scanner sc = new Scanner(System.in);
         boolean activo = true;
+        double dinero = 50;
+
+        System.out.println("Bienvenido al cajero!");
 
         while (activo) {
-            System.out.println("Bienvenido al cajero!\nIngresa una opción:\n\n1) RETIRAR DINERO.\n2) INGRESAR DINERO.\n3) VER OPERACIONES.\n4) Finalizar la operación.\n--> ");
+            System.out.println("Ingresa una opción:\n\n1) RETIRAR DINERO.\n2) INGRESAR DINERO.\n3) VER OPERACIONES.\n4) Finalizar la operación.\n--> ");
 
             int opcion = sc.nextInt();
-            double dinero = c.setSaldo(50);
             double retirar = 0;
             double ingresar = 0;
-
 
             switch (opcion) {
                 case 1: //Retirar dinero. Hay que hacer que el dinero que tengo baje y el contador de los retiros aumente
                     System.out.println("¿Cuánto quieres retirar?");
                     retirar = sc.nextDouble();
                     if (dinero <= 0) {
-                        System.out.println("No puedes retirar dinero porque no tienes.");
+                        System.out.println("No puedes retirar dinero porque no puedes retirar 0€.");
                     } else if (dinero > 0) {
                         dinero -= retirar;
-                        System.out.println("Tienes " + dinero + " €");
-                        c.setReintegrosTotales((int) (dinero -= retirar));
+                        System.out.println("Retiraste " + dinero + " €");
+                        c.setReintegrosTotales(c.getReintegrosTotales() + 1);
                     }
                     break;
+
                 case 2:
                     System.out.println("¿Cuánto quieres ingresar?");
                     ingresar = sc.nextDouble();
 
+                    while (ingresar < 0) {
+                        System.out.println("No puedes ingresar una cantidad negativa,\nintroduce una cantidad positiva: ");
+                        ingresar = sc.nextDouble();
+
+                    }
+                    while (ingresar == 0) {
+                        System.out.println("No puedes ingresar 0. Vuelve a intentarlo: ");
+                        ingresar = sc.nextDouble();
+                    }
+
+                    dinero += ingresar;
+                    c.setIngresosTotales(c.getIngresosTotales() + 1);
+                    System.out.println("Has ingresado " + ingresar + "€");
+                    System.out.println();
+
 
                     break;
+
                 case 3:
                     System.out.println("=================================================");
                     System.out.println("Saldo actual: " + dinero);
@@ -45,10 +65,23 @@ public class Principal {
                     System.out.println("=================================================");
 
                     break;
+
                 case 4:
-                    System.out.println("Hasta luego.");
-                    activo = false;
+
+                    System.out.println("¿Desea salir?  (S/N)");
+                    String respuesta = sc.next().toUpperCase();
+
+                    if (respuesta.equals("S")) {
+                        System.out.println("Saliendo del cajero...");
+                        activo = false;
+                    } else if (respuesta.equals("N")) {
+                        System.out.println("Continuando con las operaciones...");
+                        break;
+                    } else {
+                        System.out.println("Introduce el carácter válido (S -> Sí / N -> No)");
+                    }
                     break;
+
 
                 default:
                     System.out.println("Esta acción no es válida: ");
